d13453388b3ac3672f8682551024ea607788c55d
Pi_1 <kcastil3@trinity.edu>
2020-02-04 19:14:41 -0600

    Added conditionals to update.php such that color OR location can be selected

--- update.php
+++ update.php
@@ -2,12 +2,19 @@
   header("Location: .."); // Redirect back home after executing this script
 
   $myfile = fopen("vars.txt","w");
-  $txt = "The current setting is to: ";
-  fwrite($myfile, $txt); // Need permissions to write to file!
-  echo fwrite($myfile, $_POST['city']);
-  $txt = ", ";
-  echo fwrite($myfile, $txt);
-  echo fwrite($myfile, $_POST['country']); 
+  $txt = "The current setting is to ";
+  fwrite($myfile, $txt); // Note, need permissions to write to file!
+
+  // Check whether color or location was set and update txt file accordingly
+  if(! empty($_POST['city'])){
+     echo fwrite($myfile,'the location '.  $_POST['city']);
+     $txt = ", ";
+     echo fwrite($myfile, $txt);
+     echo fwrite($myfile, $_POST['country']);
+  } else{
+     echo fwrite($myfile, 'the color '. $_POST['color_val']);
+  }
+
   $fclose($myfile);
 
   exit;
